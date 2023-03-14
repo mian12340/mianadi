76aff55c0194d595080bbfec6a1a2bc9170d978a
mian12340 <125627500+mian12340@users.noreply.github.com>
2023-03-14 13:08:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("home");
+    if (x.className === "container") {
+      x.className += " responsive";
+    } else {
+      x.className = "container";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("portfolio");
+    if (x.className === "main") {
+      x.className += " responsive";
+    } else {
+      x.className = "main";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("about-us");
+    if (x.className === "about") {
+      x.className += " responsive";
+    } else {
+      x.className = "about-us";
+    }
+  }
+
+  function myFunction() {
+    var x = document.getElementById("contect");
+    if (x.className === "bg-text") {
+      x.className += " responsive";
+    } else {
+      x.className = "contect";
+    }
+  }
+
+ 
+  function myFunction() {
+    var x = document.getElementById("services");
+    if (x.className === "service") {
+      x.className += " responsive";
+    } else {
+      x.className = "service";
+    }
+  }
+
+  
